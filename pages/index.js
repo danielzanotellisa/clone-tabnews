3b63c2fea785bc5ae2f2c31bb54504bd08768891
Daniel Zanotelli <danielzanotelli12@gmail.com>
2025-11-10 21:13:59 -0300

    refact: Mudando frase da home

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-  return <h1>Vamo tomar sorvete quando eu voltar de Joao Monlevade</h1>;
+  return <h1>Aqui irá ser um espaço para uma comunidade de pessoas que amam automobilismo poderem conversar e discutir idéias! Em breve tudo vai mudar...</h1>;
 }
 
 export default Home;
