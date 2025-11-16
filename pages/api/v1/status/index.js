d199f0825b312ba71d6ea3585c84523e6d3fe1f8
Daniel Zanotelli <danielzanotelli12@gmail.com>
2025-11-16 19:26:11 -0300

    refact: Refatorando testes e atualizando retorno de status da api

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,26 @@
 import database from "/infra/database.js";
 
 async function status(request, response) {
-  const result = await database.query("SELECT 1+ 1 as sum;");
-  console.log(result.rows[0]);
-  response.status(200).json({ status: "ok" });
+  const updatedAt = new Date().toISOString();
+  const maxConnections = await database.query("SHOW max_connections;");
+  const postgresVersion = await database.query("SHOW server_version;");
+
+  const databaseName = process.env.POSTGRES_DB;
+  const activeConnections = await database.query({
+    text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1;",
+    values: [databaseName],
+  });
+
+  response.status(200).json({
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        db_version: postgresVersion.rows[0].server_version,
+        max_connections: parseInt(maxConnections.rows[0].max_connections),
+        active_connections: activeConnections.rows[0].count,
+      },
+    },
+  });
 }
 
 export default status;
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,4 +1,17 @@
 test("GET request should return 200 from api/v1/status", async () => {
   const response = await fetch("http://localhost:3000/api/v1/status");
   expect(response.status).toBe(200);
+
+  const responseBody = await response.json();
+
+  expect(responseBody.updated_at).toBeDefined();
+
+  const parsedData = new Date(responseBody.updated_at).toISOString();
+  expect(responseBody.updated_at).toEqual(parsedData);
+
+  expect(responseBody.dependencies.database.max_connections).toEqual(100);
+  expect(responseBody.dependencies.database.db_version).toBe("16.0");
+
+  expect(responseBody.dependencies.database.active_connections).toBeDefined();
+  expect(responseBody.dependencies.database.active_connections).toEqual(1);
 });
