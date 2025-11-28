ef0fb32c28f0b4db84daa967452401d58d1c77ee
Daniel Zanotelli <danielzanotelli12@gmail.com>
2025-11-28 08:52:11 -0300

    fix `prettier` style linting

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,11 @@
 function Home() {
-  return <h1>Aqui irá ser um espaço para uma comunidade de pessoas que amam automobilismo poderem conversar e discutir idéias! Em breve tudo vai mudar...</h1>;
+  return (
+    <h1>
+      Aqui irá ser um espaço para uma comunidade de pessoas que amam
+      automobilismo poderem conversar e discutir idéias! Em breve tudo vai
+      mudar...
+    </h1>
+  );
 }
 
 export default Home;
