310c6ade3a6b98fcdb8395cd107a2ccd723fe2a2
Daniel Zanotelli <danielzanotelli12@gmail.com>
2025-12-09 09:09:57 -0300

    chore: adiciona import que estava faltando

--- pages/api/v1/migrations/index.js
+++ pages/api/v1/migrations/index.js
@@ -1,5 +1,5 @@
 import migrationRunner from "node-pg-migrate";
-import { join } from "node:path";
+import { resolve } from "node:path";
 import database from "infra/database";
 
 export default async function migrations(request, response) {
