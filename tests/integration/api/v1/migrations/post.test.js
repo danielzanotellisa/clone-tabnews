5d27321a3cb9b13265c6cf19e3188edbc9c8f447
Daniel Zanotelli <danielzanotelli12@gmail.com>
2025-12-09 07:52:54 -0300

    test: renomeia os testes para usar um novo padrão com `describe`

--- tests/integration/api/v1/migrations/post.test.js
+++ tests/integration/api/v1/migrations/post.test.js
@@ -7,33 +7,42 @@ beforeAll(async () => {
   await database.query("DROP SCHEMA PUBLIC CASCADE; CREATE SCHEMA PUBLIC;");
 });
 
-test("POST to api/v1/migrations should return 200", async () => {
-  const result1 = await fetch("http://localhost:3000/api/v1/migrations", {
-    method: "POST",
-  });
-  expect(result1.status).toBe(201);
+describe("POST to api/v1/migrations", () => {
+  describe("Anonymous user", () => {
+    describe("Running pending migrations", () => {
+      test("For the first time", async () => {
+        const result1 = await fetch("http://localhost:3000/api/v1/migrations", {
+          method: "POST",
+        });
+        expect(result1.status).toBe(201);
 
-  let migrationNumber = 0;
-  fs.readdir("./infra/migrations", (err, files) => {
-    migrationNumber = files.length;
-  });
-  const migrationsRowsCount = await database.query(
-    "SELECT * FROM pgmigrations;",
-  );
-  expect(migrationsRowsCount.rows.length).toBe(migrationNumber);
+        let migrationNumber = 0;
+        fs.readdir("./infra/migrations", (err, files) => {
+          migrationNumber = files.length;
+        });
+        const migrationsRowsCount = await database.query(
+          "SELECT * FROM pgmigrations;",
+        );
+        expect(migrationsRowsCount.rows.length).toBe(migrationNumber);
+    })
+      test("For the second time", async () => {
+        const result2 = await fetch("http://localhost:3000/api/v1/migrations", {
+          method: "POST",
+        });
+      expect(result2.status).toBe(200);
+      })
+      
+      test("With methods other than POST", async () => {
+        const deleteMethod = await fetch("http://localhost:3000/api/v1/migrations", {
+          method: "DELETE",
+        });
+        expect(deleteMethod.status).toBe(405);
 
-  const result2 = await fetch("http://localhost:3000/api/v1/migrations", {
-    method: "POST",
-  });
-  expect(result2.status).toBe(200);
-
-  const deleteMethod = await fetch("http://localhost:3000/api/v1/migrations", {
-    method: "DELETE",
-  });
-  expect(deleteMethod.status).toBe(405);
-
-  const putMethod = await fetch("http://localhost:3000/api/v1/migrations", {
-    method: "PUT",
-  });
-  expect(putMethod.status).toBe(405);
+        const putMethod = await fetch("http://localhost:3000/api/v1/migrations", {
+          method: "PUT",
+        });
+        expect(putMethod.status).toBe(405);
+      })
+    })
+  })
 });
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -4,20 +4,28 @@ beforeAll(async () => {
   await orchestrator.waitForAllServices();
 });
 
-test("GET request should return 200 from api/v1/status", async () => {
-  const response = await fetch("http://localhost:3000/api/v1/status");
-  expect(response.status).toBe(200);
 
-  const responseBody = await response.json();
+describe("GET to /api/v1/status", () => {
+  describe("Anonymous user", () => {
+    test("Retrieving info from endpoint", async () => {
+      const response = await fetch("http://localhost:3000/api/v1/status");
+      expect(response.status).toBe(200);
 
-  expect(responseBody.updated_at).toBeDefined();
+      const responseBody = await response.json();
 
-  const parsedData = new Date(responseBody.updated_at).toISOString();
-  expect(responseBody.updated_at).toEqual(parsedData);
+      expect(responseBody.updated_at).toBeDefined();
 
-  expect(responseBody.dependencies.database.max_connections).toEqual(100);
-  expect(responseBody.dependencies.database.db_version).toBe("16.0");
+      const parsedData = new Date(responseBody.updated_at).toISOString();
+      expect(responseBody.updated_at).toEqual(parsedData);
 
-  expect(responseBody.dependencies.database.active_connections).toBeDefined();
-  expect(responseBody.dependencies.database.active_connections).toEqual(1);
-});
+      expect(responseBody.dependencies.database.max_connections).toEqual(100);
+      expect(responseBody.dependencies.database.db_version).toBe("16.0");
+
+      expect(responseBody.dependencies.database.active_connections).toBeDefined();
+      expect(responseBody.dependencies.database.active_connections).toEqual(1);
+    });
+  })
+})
+
+
+
