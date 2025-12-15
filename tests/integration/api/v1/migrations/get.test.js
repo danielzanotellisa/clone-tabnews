d5727650a53238870bd6d773c4d9a90d5cbb5338
Daniel Zanotelli <danielzanotelli12@gmail.com>
2025-12-15 15:49:58 -0300

    test: removendo expect

--- tests/integration/api/v1/migrations/get.test.js
+++ tests/integration/api/v1/migrations/get.test.js
@@ -9,7 +9,7 @@ describe("GET to /api/v1/migrations", () => {
   describe("Anonymous user", () => {
     test("Retrieving migration list", async () => {
       const response = await fetch("http://localhost:3000/api/v1/migrations");
-      expect(response.status).toBe(200);
+      
 
       const responseBody = await response.json();
 
