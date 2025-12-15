6e328267354b0e56b30d23880ec56fee6cfba8ff
Daniel Zanotelli <danielzanotelli12@gmail.com>
2025-12-15 12:57:12 -0300

    style: fix do prettier

--- infra/errors.js
+++ infra/errors.js
@@ -1,19 +1,19 @@
 export class InternalServerError extends Error {
-  constructor({cause}) {
+  constructor({ cause }) {
     super("Erro interno inesperado", {
-      cause
+      cause,
     });
     this.name = "InternalServerError";
     this.action = "Entre em contato com o suporte.";
     this.statusCode = 500;
   }
-  
+
   toJSON() {
     return {
       name: this.name,
       message: this.message,
       action: this.action,
-      status_code: this.statusCode
+      status_code: this.statusCode,
     };
   }
 }
--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -3,31 +3,31 @@ import { InternalServerError } from "infra/errors.js";
 async function status(request, response) {
   try {
     const updatedAt = new Date().toISOString();
-  const maxConnections = await database.query("SHOW max_connections;");
-  const postgresVersion = await database.query("SHOW server_version;");
+    const maxConnections = await database.query("SHOW max_connections;");
+    const postgresVersion = await database.query("SHOW server_version;");
 
-  const databaseName = process.env.POSTGRES_DB;
-  const activeConnections = await database.query({
-    text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1;",
-    values: [databaseName],
-  });
+    const databaseName = process.env.POSTGRES_DB;
+    const activeConnections = await database.query({
+      text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1;",
+      values: [databaseName],
+    });
 
-  response.status(200).json({
-    updated_at: updatedAt,
-    dependencies: {
-      database: {
-        db_version: postgresVersion.rows[0].server_version,
-        max_connections: parseInt(maxConnections.rows[0].max_connections),
-        active_connections: activeConnections.rows[0].count,
+    response.status(200).json({
+      updated_at: updatedAt,
+      dependencies: {
+        database: {
+          db_version: postgresVersion.rows[0].server_version,
+          max_connections: parseInt(maxConnections.rows[0].max_connections),
+          active_connections: activeConnections.rows[0].count,
+        },
       },
-    },
-  });
+    });
   } catch (error) {
     const publicErrorObj = new InternalServerError({
-      cause: error
+      cause: error,
     });
-    console.error(publicErrorObj)
-    response.status(500).json(publicErrorObj)
+    console.error(publicErrorObj);
+    response.status(500).json(publicErrorObj);
   }
 }
 
