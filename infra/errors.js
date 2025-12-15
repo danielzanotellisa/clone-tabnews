76a1cb5326974b1aa97db767b6c7b7700b235f3c
Daniel Zanotelli <danielzanotelli12@gmail.com>
2025-12-15 15:35:45 -0300

    build: cria novos erros de serivce e metodos nao permitidos

--- infra/errors.js
+++ infra/errors.js
@@ -1,11 +1,49 @@
 export class InternalServerError extends Error {
-  constructor({ cause }) {
+  constructor({ cause, statusCode }) {
     super("Erro interno inesperado", {
       cause,
     });
     this.name = "InternalServerError";
     this.action = "Entre em contato com o suporte.";
-    this.statusCode = 500;
+    this.statusCode = statusCode || 500;
+  }
+
+  toJSON() {
+    return {
+      name: this.name,
+      message: this.message,
+      action: this.action,
+      status_code: this.statusCode,
+    };
+  }
+}
+
+export class MethodNotAllowed extends Error {
+  constructor() {
+    super("Método não disponível para este endpoint");
+    this.name = "MethodNotAllowed";
+    this.action = "Utilize um método válido";
+    this.statusCode = 405;
+  }
+
+  toJSON() {
+    return {
+      name: this.name,
+      message: this.message,
+      action: this.action,
+      status_code: this.statusCode,
+    };
+  }
+}
+
+export class ServiceError extends Error {
+  constructor({ cause, message }) {
+    super(message || "Serviço indisponível no momento", {
+      cause,
+    });
+    this.name = "Service error";
+    this.action = "Entre em contato com o suporte.";
+    this.statusCode = 503;
   }
 
   toJSON() {
