d559027fb65a06448c19e5af7d10b58e3c59d268
Daniel Zanotelli <danielzanotelli12@gmail.com>
2025-12-22 09:22:27 -0300

    refactor: retira lógica de migrations do controller e importa novo model `migrator`

--- pages/api/v1/migrations/index.js
+++ pages/api/v1/migrations/index.js
@@ -1,16 +1,6 @@
-import migrationRunner from "node-pg-migrate";
-import database from "infra/database";
+import migrator from "models/migrator";
 import { createRouter } from "next-connect";
-import { resolve } from "node:path";
 import controller from "infra/controller.js";
-
-const defaultMigrationOptions = {
-  dir: resolve(process.cwd(), "infra", "migrations"),
-  dryRun: true,
-  direction: "up",
-  verbose: true,
-  migrationsTable: "pgmigrations",
-};
 
 const router = createRouter();
 
@@ -20,34 +10,16 @@ router.post(postHandler);
 export default router.handler(controller.errorHandlers);
 
 async function getHandler(request, response) {
-  let dbClient;
-  try {
-    dbClient = await database.getNewClient();
-    const pendingMigrations = await migrationRunner({
-      ...defaultMigrationOptions,
-      dbClient,
-    });
-    return response.status(200).json(pendingMigrations);
-  } finally {
-    await dbClient?.end();
-  }
+  const pendingMigrations = await migrator.listPendingMigrations();
+  return response.status(200).json(pendingMigrations);
 }
 
 async function postHandler(request, response) {
-  let dbClient;
-  try {
-    dbClient = await database.getNewClient();
-    const migratedMigrations = await migrationRunner({
-      ...defaultMigrationOptions,
-      dbClient,
-      dryRun: false,
-    });
-    if (migratedMigrations.length > 0) {
-      return response.status(201).json(migratedMigrations);
-    }
+  const migratedMigrations = await migrator.runPendingMigrations();
 
-    return response.status(200).json(migratedMigrations);
-  } finally {
-    await dbClient?.end();
+  if (migratedMigrations.length > 0) {
+    return response.status(201).json(migratedMigrations);
   }
+
+  return response.status(200).json(migratedMigrations);
 }
