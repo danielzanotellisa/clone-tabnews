bfa9b93128516e607ba004fb16442c90cb7f9bf5
Daniel Zanotelli <danielzanotelli12@gmail.com>
2026-03-08 15:26:34 -0300

    refactor: refatora controller de `sessions`

--- pages/api/v1/sessions/index.js
+++ pages/api/v1/sessions/index.js
@@ -1,9 +1,8 @@
 import { createRouter } from "next-connect";
+import * as cookie from "cookie";
 import controller from "infra/controller.js";
-import user from "models/user.js";
-import password from "models/password.js";
+import authentication from "models/authentication.js";
 import session from "models/session.js";
-import { UnprocessableEntity, UnauthorizedError } from "infra/errors.js";
 const router = createRouter();
 
 router.post(postHandler);
@@ -13,7 +12,22 @@ export default router.handler(controller.errorHandlers);
 async function postHandler(request, response) {
   const userInput = request.body;
 
-  await session.createSession(userInput);
+  const authenticatedUser = await authentication.getAuthenticatedUser(
+    userInput?.email,
+    userInput?.password,
+  );
 
-  return response.status(200).json({});
+  const createdSession = await session.create(authenticatedUser.id);
+
+  const setCookie = cookie.serialize("session_id", createdSession.token, {
+    path: "/",
+    httpOnly: true,
+    maxAge: session.EXPIRATION_IN_MILLISECONDS / 1000,
+    secure: process.env.NODE_ENV === "production",
+    sameSite: "strict",
+  });
+
+  response.setHeader("Set-Cookie", setCookie);
+
+  return response.status(201).json(createdSession);
 }
