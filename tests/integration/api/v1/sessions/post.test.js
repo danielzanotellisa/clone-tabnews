f93847e4b7be9f311b248dc81b7a1ed08852bb67
Daniel Zanotelli <danielzanotelli12@gmail.com>
2026-03-08 15:26:47 -0300

    test: modifica os testes no caso de sucesso

--- tests/integration/api/v1/sessions/post.test.js
+++ tests/integration/api/v1/sessions/post.test.js
@@ -3,6 +3,8 @@ import { version as uuidVersion } from "uuid";
 import user from "models/user.js";
 import password from "models/password.js";
 import { UnauthorizedError } from "infra/errors.js";
+import session from "models/session.js";
+import setCookieParser from "set-cookie-parser";
 
 beforeAll(async () => {
   await orchestrator.waitForAllServices();
@@ -14,7 +16,8 @@ describe("POST to api/v1/sessions", () => {
   describe("Anonymous user", () => {
     test("With valid credentials", async () => {
       const createdUser = await orchestrator.createUser({
-        password: "123456",
+        email: "tudo.correto@email.com",
+        password: "tudocorreto",
       });
       const response = await fetch("http://localhost:3000/api/v1/sessions", {
         method: "POST",
@@ -22,16 +25,49 @@ describe("POST to api/v1/sessions", () => {
           "Content-Type": "application/json",
         },
         body: JSON.stringify({
-          email: `${createdUser.email}`,
-          password: "123456",
+          email: "tudo.correto@email.com",
+          password: "tudocorreto",
         }),
       });
 
-      expect(response.status).toBe(200);
+      expect(response.status).toBe(201);
 
       const responseBody = await response.json();
       expect(responseBody).toEqual({
+        id: responseBody.id,
+        user_id: createdUser.id,
         token: responseBody.token,
+        expires_at: responseBody.expires_at,
+        created_at: responseBody.created_at,
+        updated_at: responseBody.updated_at,
+      });
+
+      expect(uuidVersion(responseBody.id)).toBe(4);
+
+      expect(Date.parse(responseBody.expires_at)).not.toBeNaN();
+      expect(Date.parse(responseBody.created_at)).not.toBeNaN();
+      expect(Date.parse(responseBody.updated_at)).not.toBeNaN();
+
+      const expiresAt = new Date(responseBody.expires_at);
+      const createdAt = new Date(responseBody.created_at);
+
+      const expirationInMilliSeconds = expiresAt - createdAt;
+
+      expect(
+        session.EXPIRATION_IN_MILLISECONDS - expirationInMilliSeconds,
+      ).toBeLessThan(1000);
+
+      const parsedSetCookie = setCookieParser(response, {
+        map: true,
+      });
+      expect(parsedSetCookie.session_id).toBeDefined();
+      expect(parsedSetCookie.session_id).toEqual({
+        name: "session_id",
+        value: responseBody.token,
+        maxAge: session.EXPIRATION_IN_MILLISECONDS / 1000,
+        httpOnly: true,
+        path: "/",
+        sameSite: "Strict",
       });
     });
     test("With incorrect email but correct password", async () => {
