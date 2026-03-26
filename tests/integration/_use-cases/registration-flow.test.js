188d4cc7e9baa3942faa13b2bc791a8bed5bfd55
Daniel Zanotelli <danielzanotelli12@gmail.com>
2026-03-25 21:54:43 -0300

    test: cria suite de testes de registration flow

---
+++ tests/integration/_use-cases/registration-flow.test.js
@@ -0,0 +1,48 @@
+import password from "models/password";
+import orchestrator from "tests/orchestrator.js";
+
+beforeAll(async () => {
+  await orchestrator.waitForAllServices();
+  await orchestrator.clearDatabase();
+  await orchestrator.runPendingMigrations();
+  await orchestrator.deleteEmails();
+});
+
+describe("Use case: Registration Flow (all successful)", () => {
+  test("Create user account", async () => {
+    const createUserResponse = await fetch(
+      "http://localhost:3000/api/v1/users",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          username: "RegistrationFlow",
+          email: "registration@flow.com",
+          password: "registrationflow",
+        }),
+      },
+    );
+
+    expect(createUserResponse.status).toBe(201);
+
+    const createUserResponseBody = await createUserResponse.json();
+
+    expect(createUserResponseBody).toEqual({
+      id: createUserResponseBody.id,
+      username: "registration@flow.com",
+      password: createUserResponseBody.password,
+      created_at: createUserResponseBody.created_at,
+      updated_at: createUserResponseBody.updated_at,
+    });
+  });
+
+  test("Receive activation email", async () => {});
+
+  test("Activate account", async () => {});
+
+  test("Login", async () => {});
+
+  test("Get user information", async () => {});
+});
