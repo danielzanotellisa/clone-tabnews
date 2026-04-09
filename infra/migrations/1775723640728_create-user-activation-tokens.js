a586efa39f164cb453e0c601d4bcb2427981355d
Daniel Zanotelli <danielzanotelli12@gmail.com>
2026-04-09 06:09:37 -0300

    build: cria nova migration `create-user-activation-token`

---
+++ infra/migrations/1775723640728_create-user-activation-tokens.js
@@ -0,0 +1,35 @@
+exports.shorthands = undefined;
+
+exports.up = (pgm) => {
+  pgm.createTable("user_activation_tokens", {
+    id: {
+      type: "uuid",
+      primaryKey: true,
+      default: pgm.func("gen_random_uuid()"),
+    },
+    user_id: {
+      type: "uuid",
+      notNull: true,
+    },
+    used_at: {
+      type: "timestamp",
+      notNull: false,
+    },
+    expirest_at: {
+      type: "timestamp",
+      notNull: true,
+    },
+    created_at: {
+      type: "timestamp",
+      notNull: true,
+      default: pgm.func("now()"),
+    },
+    updated_at: {
+      type: "timestamp",
+      notNull: true,
+      default: pgm.func("now()"),
+    },
+  });
+};
+
+exports.down = false;
