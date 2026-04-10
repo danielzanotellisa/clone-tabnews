0d9002a344774a7558dce8bd1b4bea2ba0ad9516
Daniel Zanotelli <danielzanotelli12@gmail.com>
2026-04-10 14:01:49 -0300

    feat: adiciona nova rota `api/v1/activations/[token_id]` para ativar um usuário

---
+++ pages/api/v1/activations/[token_id]/index.js
@@ -0,0 +1,20 @@
+import { createRouter } from "next-connect";
+import controller from "infra/controller.js";
+import activation from "models/activation.js";
+const router = createRouter();
+
+router.patch(patchHandler);
+
+export default router.handler(controller.errorHandlers);
+
+async function patchHandler(request, response) {
+  const activationToken = request.query.token_id;
+
+  const validACtivationToken =
+    await activation.findOneValidById(activationToken);
+  const usedActivationToken = await activation.markAsUsed(activationToken);
+
+  await activation.activateUserByUserId(validACtivationToken.user_id);
+
+  return response.status(200).json(usedActivationToken);
+}
