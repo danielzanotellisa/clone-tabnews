5ea8fe704ea66084d45a611a5794d5f5ca09805b
Daniel Zanotelli <danielzanotelli12@gmail.com>
2026-04-13 18:46:51 -0300

    feat: adiciona nova middleware para travar usuários de ativarem a conta duas vezes

--- pages/api/v1/activations/[token_id]/index.js
+++ pages/api/v1/activations/[token_id]/index.js
@@ -3,6 +3,8 @@ import controller from "infra/controller.js";
 import activation from "models/activation.js";
 const router = createRouter();
 
+router.use(controller.injectAnonymousOrUser);
+router.get(controller.canRequest("read:activation_token"), patchHandler);
 router.patch(patchHandler);
 
 export default router.handler(controller.errorHandlers);
